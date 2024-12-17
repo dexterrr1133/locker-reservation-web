cfc83378569761ad034a1b179fbec72c20e5e083
dex <linusfigueroa.tech@gmail.com>
2024-12-17 13:53:14 +0800

    added dialog boxes when reserving  for all locker sizes

--- nu-locker-reservation/src/app/(public)/tall_lockers/page.tsx
+++ nu-locker-reservation/src/app/(public)/tall_lockers/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/features/header';
 import TallLockers from '@/components/lockers/tall_locker_list';
 import ProtectedRoutes from '@/components/protectedRoutes';
 
 const Large_Locker = () => {
   return (
     <ProtectedRoutes>
+      <Header />
       <TallLockers />
     </ProtectedRoutes>
   );
